566eeecbc58c414fe57a690697bfd612a48dfbac
Jamie Norrish <jamie@artefact.org.nz>
2012-08-23 16:01:20 +1200

    Reordered files into a layout suitable for use with distutils. Added distutils setup script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='Bacalhau',
+      version = '1.0',
+      author = 'Miguel Vieira',
+      author_email = 'jose.m.vieira@kcl.ac.uk',
+      url = 'https://github.com/kcl-ddh/bacalhau',
+      packages = ['bacalhau'],
+      scripts = ['scripts/bacalhau'],
+      )
