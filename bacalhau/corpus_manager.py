c5184e1e02b1b4eaf7758da6c7ae11caa04701b8
jmiguelv <jose.m.vieira@kcl.ac.uk>
2012-08-31 16:46:30 +0100

    Added the function generate_topic_tree that will call all the necessary functions to generate a topic tree from a corpus. Removed the creation of intermediate pickle files.

--- bacalhau/corpus_manager.py
+++ bacalhau/corpus_manager.py
@@ -4,7 +4,6 @@ from operator import itemgetter
 import networkx
 import nltk
 import os
-import pickle
 
 
 class CorpusManager(object):
@@ -26,81 +25,13 @@ class CorpusManager(object):
         self._textcollection = None
         self._texts = {}
 
-    def generate(self):
-        """Generates topic tree: creates hypernym paths for the target terms,
-        generates topic tree for the hypernym paths, compresses the topic
-        tree."""
-        self.extract()
-
-        hypernyms_dict = {}
-        tree = networkx.DiGraph()
-
-        for text in self._texts.values():
-            hypernyms_dict = dict(hypernyms_dict.items() + \
-                    text._hypernyms_dict.items())
-
-        for hypernym in hypernyms_dict.values():
-            if len(hypernym) > 0:
-                tree.add_nodes_from(hypernym)
-                tree.node[hypernym[0]]['is_leaf'] = True
-                tree.node[hypernym[len(hypernym) - 1]]['is_root'] = True
-                hypernym.reverse()
-                tree.add_path(hypernym)
-
-        tree = self._compress_and_prune_tree(tree, [])
-
-        return tree
-
-    def extract(self):
-        """Extracts target tems from the texts: selects nouns, computes tf.idf,
-        merges the target terms into a unique list."""
+    def generate_topic_tree(self, n_target_terms=10, nodes_to_prune=[],
+            min_children=2):
+        """Generates the topic tree for the corpus."""
         self.prepare()
+        self.extract(n_target_terms)
 
-        self._corpus = nltk.corpus.PlaintextCorpusReader(self._work_path, '.*')
-        self._textcollection = nltk.text.TextCollection(self._corpus)
-
-        for f in self._corpus.fileids():
-            tf_idf_dict = {}
-
-            for w in self._corpus.words(fileids=f):
-                tf_idf_dict[w] = self._textcollection.tf_idf(w,
-                        self._textcollection)
-
-            text = self._texts[f]
-            text._tf_idf_dict = tf_idf_dict
-
-            tf_idf_dict = sorted(tf_idf_dict.iteritems(), key=itemgetter(1),
-                    reverse=True)
-
-            tf_idf_file = open(os.path.join(self._work_path, f + '-tf_id.pkl'),
-                'wb')
-            pickle.dump(tf_idf_dict, tf_idf_file)
-            tf_idf_file.close()
-
-            hypernyms_dict = {}
-
-            for idx, item in enumerate(tf_idf_dict):
-                if idx >= 10:
-                    break
-
-                word = item[0]
-                hypernyms_dict[word] = []
-                hypernyms_dict[word].append(word)
-
-                synsets = nltk.corpus.wordnet.synsets(word)
-
-                while len(synsets) > 0:
-                    syn = synsets[0]
-                    name = syn.name
-                    hypernyms_dict[word].append(name)
-                    synsets = syn.hypernyms()
-
-            text._hypernyms_dict = hypernyms_dict
-
-            hypernyms_file = open(os.path.join(self._work_path,
-                f + '-syn.pkl'), 'wb')
-            pickle.dump(hypernyms_dict, hypernyms_file)
-            hypernyms_file.close()
+        return self.generate(nodes_to_prune, min_children)
 
     def prepare(self):
         """Prepares the corpus for the topic tree generation."""
@@ -118,14 +49,77 @@ class CorpusManager(object):
                 for text in texts:
                     self._texts[text._key] = text
 
-    def _compress_and_prune_tree(self, tree, nodes_to_prune):
+    def extract(self, n_target_terms=10):
+        """Extracts target tems from the texts: selects nouns, computes tf.idf,
+        and creates hypernym paths for the target terms."""
+        self._corpus = nltk.corpus.PlaintextCorpusReader(self._work_path, '.*')
+        self._textcollection = nltk.text.TextCollection(self._corpus)
+
+        for f in self._corpus.fileids():
+            tf_idf_dict = {}
+
+            for w in self._corpus.words(fileids=f):
+                tf_idf_dict[w] = self._textcollection.tf_idf(w,
+                        self._textcollection)
+
+            text = self._texts[f]
+            text._tf_idf_dict = tf_idf_dict
+
+            tf_idf_dict = sorted(tf_idf_dict.iteritems(), key=itemgetter(1),
+                    reverse=True)
+
+            hypernyms_dict = {}
+
+            for idx, item in enumerate(tf_idf_dict):
+                if idx >= n_target_terms:
+                    break
+
+                word = item[0]
+                hypernyms_dict[word] = []
+                hypernyms_dict[word].append(word)
+
+                synsets = nltk.corpus.wordnet.synsets(word)
+
+                while len(synsets) > 0:
+                    syn = synsets[0]
+                    name = syn.name
+                    hypernyms_dict[word].append(name)
+                    synsets = syn.hypernyms()
+
+            text._hypernyms_dict = hypernyms_dict
+
+    def generate(self, nodes_to_prune=[], min_children=2):
+        """Generates topic tree: creates hypernym paths for the target terms,
+        generates topic tree for the hypernym paths, compresses the topic
+        tree."""
+        hypernyms_dict = {}
+        tree = networkx.DiGraph()
+
+        for text in self._texts.values():
+            hypernyms_dict = dict(hypernyms_dict.items() + \
+                    text._hypernyms_dict.items())
+
+        for hypernym in hypernyms_dict.values():
+            if len(hypernym) > 0:
+                tree.add_nodes_from(hypernym)
+                tree.node[hypernym[0]]['is_leaf'] = True
+                tree.node[hypernym[len(hypernym) - 1]]['is_root'] = True
+                hypernym.reverse()
+                tree.add_path(hypernym)
+
+        tree = self._compress_and_prune_tree(tree, nodes_to_prune,
+                min_children)
+
+        return tree
+
+    def _compress_and_prune_tree(self, tree, nodes_to_prune, min_children):
         """Compresses the tree using the castanet algorithm:
         1. starting from the leaves, recursively eliminate a parent that has
         fewer than 2 children, unless the parent is the root
         2. eliminate a child whose name appears within the parent's name."""
         for n in tree.nodes(data=True):
             if 'is_leaf' in n[1]:
-                tree = self._eliminate_parents(tree, n[0])
+                tree = self._eliminate_parents(tree, n[0], min_children)
 
         for n in tree.nodes(data=True):
             if 'is_leaf' in n[1]:
@@ -135,15 +129,15 @@ class CorpusManager(object):
 
         return tree
 
-    def _eliminate_parents(self, tree, node, k=2):
+    def _eliminate_parents(self, tree, node, min_children=2):
         """Recursively eliminates a parent of the current node that has fewer
-        than k children, unless the parent is the root."""
+        than min_children children, unless the parent is the root."""
         for p in tree.predecessors(node):
             if tree.has_node(p):
                 n_children = len(tree.out_edges(p))
                 has_parent = len(tree.predecessors(p)) > 0
 
-                if n_children < k and has_parent:
+                if n_children < min_children and has_parent:
                     ancestor = tree.predecessors(p)[0]
                     children = tree.successors(p)
 
