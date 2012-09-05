f19df68ef85c60fe5b2f26e344db91b1e25c2eef
jmiguelv <jose.m.vieira@kcl.ac.uk>
2012-09-05 10:31:02 +0100

    Added abstract method _get_texts.

--- bacalhau/document/base.py
+++ bacalhau/document/base.py
@@ -7,7 +7,7 @@ class Document:
     should extend this class and override the abstract methods."""
     __metaclass__ = abc.ABCMeta
 
-    def __init__ (self, filepath, tokenizer, stopwords):
+    def __init__(self, filepath, tokenizer, stopwords):
         """Creates a new Document for the given file path."""
         self._path = os.path.abspath(filepath)
         self._document_id = os.path.splitext(os.path.basename(self._path))[0]
@@ -17,9 +17,14 @@ class Document:
         self._texts = self._get_texts(self._document_id)
 
     @abc.abstractmethod
-    def get_term_data (self):
+    def get_term_data(self):
         """Returns term data for each `Text` within this document."""
         return
 
-    def get_text_count (self):
+    def get_text_count(self):
         return len(self._texts)
+
+    @abc.abstractmethod
+    def _get_texts(self, document_id):
+        """Returns a list of `Text` objects within this document."""
+        return
