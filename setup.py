9685ca7a709fc451191c7db749ef6008f8e9d77e
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2012-09-12 14:06:13 +0100

    Added list of required packages to the setup script.

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@
 
 from distutils.core import setup
 
-
 setup(name='Bacalhau',
-      version = '1.0',
-      author = 'Miguel Vieira',
-      author_email = 'jose.m.vieira@kcl.ac.uk',
-      url = 'https://github.com/kcl-ddh/bacalhau',
-      packages = ['bacalhau', 'bacalhau.corpus', 'bacalhau.document'],
-      scripts = ['scripts/bacalhau'],
-      )
+        version='1.0',
+        author='Miguel Vieira',
+        author_email='jose.m.vieira@kcl.ac.uk',
+        url='https://github.com/kcl-ddh/bacalhau',
+        packages=['bacalhau', 'bacalhau.corpus', 'bacalhau.document'],
+        scripts=['scripts/bacalhau'],
+        requires=['numpy', 'pyyaml', 'nltk', 'networkx', 'lxml',
+            'pygraphvis'],
+        )
