abc73c0e7f6886bf1aa1ae0615eb8ab37dbc7936
jmiguelv <jose.m.vieira@kcl.ac.uk>
2012-09-13 11:18:19 +0100

    Implemented TopicTree unit test.

--- tests/test_topic_tree.py
+++ tests/test_topic_tree.py
@@ -26,11 +26,45 @@ class TestTopicTree(unittest.TestCase):
         compressed_number_of_nodes = self.tree.number_of_nodes()
         self.assertLess(compressed_number_of_nodes, number_of_nodes)
 
+    def test__eliminate_parents(self):
+        number_of_nodes = self.tree.number_of_nodes()
+        self.tree._eliminate_parents('g', min_children=1)
+        n = self.tree.number_of_nodes()
+        self.assertEqual(n, number_of_nodes)
+        self.tree._eliminate_parents('g', min_children=2)
+        n = self.tree.number_of_nodes()
+        self.assertLess(n, number_of_nodes)
+
+    def test__eliminate_child_with_parent_name(self):
+        number_of_nodes = self.tree.number_of_nodes()
+        self.tree._eliminate_child_with_parent_name('c')
+        n = self.tree.number_of_nodes()
+        self.assertLess(n, number_of_nodes)
+        self.assertFalse(self.tree.has_node('c'))
+
+    def test_prune(self):
+        number_of_nodes = self.tree.number_of_nodes()
+        self.tree.prune(['a'])
+        pruned_number_of_nodes = self.tree.number_of_nodes()
+        self.assertLess(pruned_number_of_nodes, number_of_nodes)
+        self.assertFalse(self.tree.has_node('a'))
+
     def test_render(self):
         filename = 'test.svg'
 
         self.addCleanup(os.remove, filename)
         self.tree.render(filename)
+
+        try:
+            open(filename, 'r')
+        except IOError:
+            self.fail()
+
+    def test_to_json(self):
+        filename = 'test.js'
+
+        self.addCleanup(os.remove, filename)
+        self.tree.to_json(filename)
 
         try:
             open(filename, 'r')
